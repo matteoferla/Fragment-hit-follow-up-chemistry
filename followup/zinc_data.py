47db08cb2ff3d5609215a87068c6de9772f6d465
matteoferla <matteo.ferla@gmail.com>
2023-07-07 17:35:12 +0100

    :truck: files added

---
+++ followup/zinc_data.py
@@ -0,0 +1,95 @@
+import requests, collections, contextlib, json
+from bs4 import BeautifulSoup
+
+
+class ZincInformer(collections.abc.MutableMapping):
+    """
+    A simple class to get Zinc info.
+
+    A class that stores the retieved values —in ``.data`` (``.dump`` and ``.load`` to store).
+    The values can be accessed as a subscript or by calling the instance,
+    the latter captures errors declared during initialisation by the argument ``suppressed_exception``.
+
+    .. code-block::python
+        zinfo = ZincInformer()
+        data: pd.DataFrame = series.apply(zinfo)
+    """
+
+    def __init__(self, suppressed_exception=Exception):
+        self.data = {}
+        self.suppressed_exception = suppressed_exception
+
+    def __getitem__(self, zinc_id):
+        if zinc_id not in self.data:
+            soup = self.get_soup(zinc_id)
+            self.data[zinc_id] = self.get_zinc_info(zinc_id, soup)
+        return self.data[zinc_id]
+
+    def __call__(self, zinc_id):
+        with contextlib.suppress(self.suppressed_exception):
+            return self[zinc_id]
+        self[zinc_id] = {}
+        return {}
+
+    def __setitem__(self, zinc_id: str, info: dict):
+        self.data[zinc_id] = info
+
+    def __delitem__(self, zinc_id):
+        del self.data[zinc_id]
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def dump(self, filename: str = 'zinc.json'):
+        with open(filename, 'w') as fh:
+            json.dump(self.data, fh)
+
+    def load(self, filename: str = 'zinc.json'):
+        with open(filename, 'r') as fh:
+            self.data = json.load(fh)
+
+    # ======== specific methods
+
+    @classmethod
+    def get_soup(self, zinc_id: str) -> BeautifulSoup:
+        """parse HTML. Return a soup"""
+        response = requests.get(f'https://zinc.docking.org/substances/{zinc_id}')
+        response.raise_for_status()
+        return BeautifulSoup(response.text, 'html.parser')
+
+    @classmethod
+    def get_dl(self, soup: BeautifulSoup) -> dict:
+        """
+        Data tables are organised in dl entries, dt headers and dd fields.
+        """
+        keys, values = [], []
+        for dl in soup.findAll("dl"):
+            for dt in dl.findAll("dt"):
+                keys.append(dt.text.strip())
+            for dd in dl.findAll("dd"):
+                values.append(dd.text.strip())
+        return dict(zip(keys, values))
+
+    @classmethod
+    def polísh(self, dl: dict) -> dict:
+        """
+        There's a bunch of words that get in the way...
+        """
+        return {k.replace('Bioactive', '').replace('Natural Products', '').replace('Building Blocks', '').strip(): v for
+                k, v in dl.items()}
+
+    @classmethod
+    def get_zinc_info(self, zinc_id, soup):
+        """
+        These fields ought to always exist!
+        """
+        return {'query_name': zinc_id,
+                'title': soup.title.text.strip(),
+                'SMILES': soup.find('input', dict(id="substance-smiles-field")).attrs['value'].strip(),
+                'inchi': soup.find('input', dict(id="substance-inchi-field")).attrs['value'].strip(),
+                'inchikey': soup.find('input', dict(id="substance-inchikey-field")).attrs['value'].strip(),
+                **self.polísh(self.get_dl(soup))
+                }
---
+++ questionnaire.md
@@ -0,0 +1,48 @@
+## Follow-up Design Questionnaire
+
+### Data availability
+Is it on Fragalysis? What is it’s name?
+
+If not, could you share the PDBs and SMILES via OneDrive?
+If you do not have SMILES, do you have anything similar? It is essential to add bond order and fix protonation which is painful otherwise.
+
+Do you have a reference structure?
+
+### Oligomeric cardinality
+What is the oligomeric state/cardinality of the protein in vivo and in your crystal?
+If biologically a monomer, but packs in as an asymmetric dimer, are any differences between chains?
+
+### Activity
+What residues are important? (I can find out…)
+
+What does your protein do? (I can check on Uniprot)
+
+Is the substrate known? If protein but the binding isn’t know, do you need an AF2 model (ColabFold)?
+
+Any PDBs of interest? Should I run my code to steal ligands from homologues?
+
+### Hits
+Do you have hits you are very interesting in / confident in that are in the active site?
+Do any of the hits alter the protein structure? I.e. the protein is in a conformation like apo vs substrate-bound vs transition-state-bound vs product-bound vs PTM
+(Does your protein behave that way and should any modelling be done?)
+
+### Ideas
+Do you have any ideas you would like tested for expected pose?
+Say linking A and B via an amide.
+Compounds scribbled on paper is fine, but you can use the editor in say https://arthor.docking.org/index.html
+
+### Vendors
+Are all standard vendors okay? Enamine REAL & co. are the go to, but you might have specific unusual vendors in mind.
+Time frame
+When is your next visit scheduled?
+
+### Outcome
+Can I store the results on GitHub publicly? Privately and I add you?
+How much curation of the output is sought?
+If a shortlist is sought
+
+* how many?
+* How daring? (i.e. “Honestly, the next will be the last visit and no further elaborations will happen, so do go all in” or “I want to better explore binding space for the ultimate drug”)
+
+How much technical information would you like back?
+
